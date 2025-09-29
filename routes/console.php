22eaf13f23631494a4d7603eb7616257003ba9f0
David Ekechi <davemichael700@gmail.com>
2025-09-29 20:14:03 +0100

    Removed nunomaduro/larastan

--- routes/console.php
+++ routes/console.php
@@ -5,6 +5,6 @@ declare(strict_types=1);
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('app:fetch-and-store-news-articles')
-    ->everyMinute()
+    ->hourly()
     ->withoutOverlapping()
     ->runInBackground();
